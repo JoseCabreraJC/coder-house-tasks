bdcd1b01eaa3c0a0bce3a1e7d1be57f0742c9c52
Jose Cabrera <josecabrera@cloud.upwork.com>
2022-07-04 21:01:36 -0300

    improved camera change method

--- Assets/Scripts/CameraChange.cs
+++ Assets/Scripts/CameraChange.cs
@@ -6,24 +6,27 @@ public class CameraChange : MonoBehaviour
 {
     public GameObject freeLookCam;
     public GameObject clearShotCam;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            ChangeActiveCamera();
+        }
+    }
+    void ChangeActiveCamera() 
+    {
+        if (freeLookCam.activeInHierarchy)
         {
             freeLookCam.SetActive(false);
             clearShotCam.SetActive(true);
         }
-        if (Input.GetKeyDown(KeyCode.X))
+        else
         {
             clearShotCam.SetActive(false);
             freeLookCam.SetActive(true);
         }
     }
+
 }
