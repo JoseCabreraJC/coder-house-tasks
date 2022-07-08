db4456c077dc421657116b70f012bb1fa2c19329
Jose Cabrera <josecabrera@cloud.upwork.com>
2022-07-07 22:20:40 -0300

    deleted internet move method

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,23 +4,13 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    Vector3 Vec;
     public float hor;
     public float ver;
     public float speed = 2;
     // Update is called once per frame
     void Update()
     {
-        //MoverInternet();
         MoverClase();
-    }
-    public void MoverInternet()
-    {
-        Vec = transform.localPosition;
-        Vec.y += Input.GetAxis("Jump") * Time.deltaTime * 20;
-        Vec.x += Input.GetAxis("Horizontal") * Time.deltaTime * 20;
-        Vec.z += Input.GetAxis("Vertical") * Time.deltaTime * 20;
-        transform.localPosition = Vec;
     }
     public void MoverClase()
     {
